3501f4063477ae0491a4566e843128eeb6c050b4
Adam <adamsopbb@gmail.com>
2014-08-10 15:41:44 +0200

    added migrations with south

---
+++ bbsite/bbapp/migrations/0001_initial.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'News'
+        db.create_table(u'bbapp_news', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('title', self.gf('django.db.models.fields.CharField')(max_length=100)),
+            ('text', self.gf('django.db.models.fields.TextField')()),
+            ('pub_date', self.gf('django.db.models.fields.DateTimeField')()),
+        ))
+        db.send_create_signal(u'bbapp', ['News'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'News'
+        db.delete_table(u'bbapp_news')
+
+
+    models = {
+        u'bbapp.news': {
+            'Meta': {'object_name': 'News'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'pub_date': ('django.db.models.fields.DateTimeField', [], {}),
+            'text': ('django.db.models.fields.TextField', [], {}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        }
+    }
+
+    complete_apps = ['bbapp']
--- bbsite/bbsite/settings.py
+++ bbsite/bbsite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bbapp',
+    'south',
 )
 
 MIDDLEWARE_CLASSES = (
